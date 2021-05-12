ada18a3ac70505d0e563193703ef5bf22ab67266
Terri Ann <terri.ann.s@gmail.com>
2021-05-11 23:16:43 -0400

    Replace configuration file with sample file

---
+++ inc/sitesToTest.sample.js
@@ -0,0 +1,13 @@
+// Rename this file to `sitesToTest.js` which is ignored in this skeleton repo.
+
+module.exports = {
+  "VIP Docs": {
+    label: "Vip Docs",
+    productionBaseUrl: "https://example.com/",
+    nonProductionBaseUrl: "https://staging.example.com/",
+
+    pathsToTest: ["/", "/about"],
+    // Global selectors to hide on all pages.
+    hideSelectors: [".cookie-banner"],
+  },
+};
